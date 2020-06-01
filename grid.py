9be8839549d9c0464068850768c9288caa803fb8
gririnen <42743133+gririnen@users.noreply.github.com>
2020-06-01 18:36:34 +0300

    Add files via upload

--- grid.py
+++ grid.py
@@ -3,6 +3,6 @@ def genGrid(gridSide):
   for i in range(gridSide):
     temp = []
     for j in range(gridSide):
-      temp.append(false)
+      temp.append(False)
     grid.append(temp)
   return grid
---
+++ playGround.py
@@ -0,0 +1,9 @@
+import dot
+import grid
+#import graphicsHandler
+
+
+gridSide = int(input())
+
+grid = grid.genGrid(gridSide)
+print(grid)
