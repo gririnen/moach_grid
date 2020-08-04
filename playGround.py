db798d1c853b0cf12e0b2b2c10533dd04e5da702
gririnen <42743133+gririnen@users.noreply.github.com>
2020-08-04 13:11:35 +0300

    Add files via upload

--- playGround.py
+++ playGround.py
@@ -1,11 +1,13 @@
+
 import dot
 import random
-import graphicsHandler as gH
+#import graphicsHandler as gH
+import math
 
 # generates a grid with the input side length 
 gridSide = int(input("grid side:"))
 dot.genGrid(gridSide)
-gH.initGraphics(dot.returnGrid())
+#gH.initGraphics(dot.returnGrid())
 
 dotLs = []
 def genMidSquare(side):
@@ -14,19 +16,24 @@ def genMidSquare(side):
 	for i in range(side**2):
 		dotLs.append(dot.part([int(leftCorn + i/side), leftCorn + i % side]))
 
-genMidSquare(5)
+def genMidCircle(rad):
+	cntr = int(gridSide/2)
+	for i in range(gridSide):
+		for j in range(gridSide):
+			if((i - cntr)**2 + (j - cntr)**2 <=rad**2):
+				dotLs.append(dot.part([i, j]))
 
-gH.genGraphics(dot.returnGrid())
+genMidCircle(4)
+
+#gH.genGraphics(dot.returnGrid())
 counter = 0
-while dot.killProgram == False:
+while not dot.killProgram:
 	random.shuffle(dotLs) # important for not creating a bias twords one side
+	counter += 1
 	for d in dotLs:
-		if(dot.killProgram == True):
+		if(dot.killProgram):
 			break
-		counter += 1
 		x,y = d.x,d.y
 		d.step(dot.genStep())
-		gH.drawStep(x,y,d.x,d.y)
-	#gH.genGraphics(dot.returnGrid())
+		#gH.drawStep(x,y,d.x,d.y)
 print("steps:", counter)
-print(dot.returnGrid())
