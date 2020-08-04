97b18af88b8442c7beb89835f6aa312054065135
gririnen <42743133+gririnen@users.noreply.github.com>
2020-08-04 13:19:22 +0300

    Update configFile.py

--- configFile.py
+++ configFile.py
@@ -2,6 +2,6 @@ config = {
 	"allow2dotsInTheSamePlace": False, # taken care of in dot
 	"colorSteppedPlaces": False, #taken care of in graphicsHandler
 	"startingShape": "square", #needs to be read in your program
-	"radiusOrSide": 20, #needs to be read in your program
+	"diameterOrSide": 20, #needs to be read in your program
 	"gridSide": 40 #needs to be read in your program
 }
