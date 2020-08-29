ca4502dac2d177cec8bc2ede597a7e6b317f829e
toxicblacker <66261313+toxicblacker@users.noreply.github.com>
2020-08-29 20:40:58 +0300

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,5 +1,7 @@
-data = []
+data = [] #two dimentional list that saves the locations of the particles for easy access
 def genGrid(gridSide):
+	"""genGrid(int) -> void
+	makes the data grid gridSide long on both dimentions. data must be empty"""
 	for i in range(gridSide):
 		temp = []
 		for j in range(gridSide):
